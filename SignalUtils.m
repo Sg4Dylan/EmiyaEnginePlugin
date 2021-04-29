1b3786c070940c3d4cc77e853cf31c72eda267c8
Sg4Dylan <i@acg.works>
2021-04-29 16:48:25 +0800

    Fix: correct the usage of fft & filter

--- SignalUtils.m
+++ SignalUtils.m
@@ -1,8 +1,8 @@
-classdef SignalUtils
+classdef SignalUtils < handle
     %SIGNALUTILS
     %   Audio processing
     
-    properties
+    properties (Access = private)
         EdgeFreq;
         ChanSum;
         AccCount;
@@ -10,13 +10,13 @@ classdef SignalUtils
     
     properties (Constant)
         % EnvelopeDetect parameter
-        TGain = 85;
-        SlideWindow = 8;
-        ChanSize = 1024;
+        TGain = 50;
+        SlideWindow = 4;
+        FFTSize = 512;
     end
     
     methods (Access = private)
-        function utils = EnvelopeDetect(utils,Input,InputSR,SrcFreq)
+        function utils = EnvelopeDetect(utils,Input,InputSR,DstFreq)
             % Must suit for fixed size
             if size(Input) ~= size(utils.ChanSum)
                 return
@@ -30,65 +30,80 @@ classdef SignalUtils
             % Update mean value
             NewChan = abs(fft(Chan));
             utils.ChanSum = (utils.AccCount.*utils.ChanSum+NewChan)/(utils.AccCount+1);
+            utils.AccCount = utils.AccCount + 1;
+            % Post process FFT result
+            ChanFFT = utils.ChanSum/(2*utils.FFTSize);
+            ChanFFT = 2*ChanFFT(1:utils.FFTSize);
             % Calculate max value
-            ChanMax = max(utils.ChanSum);
+            ChanMax = max(ChanFFT);
             % Find edge frequence
             ProbFreq = 0;
-            for p = 1:utils.ChanSize
+            for p = 1:utils.FFTSize
                 SlideMean = 0;
-                if (p+utils.SlideWindow) <= utils.ChanSize
-                    SlideMean = mean(utils.ChanSum(p:p+utils.SlideWindow));
+                if (p+utils.SlideWindow) <= utils.FFTSize
+                    SlideMean = mean(ChanFFT(p:p+utils.SlideWindow));
                 else
-                    SlideMean = mean(utils.ChanSum(p:utils.ChanSize))*(utils.SlideWindow/(utils.ChanSize-p));
+                    SlideMean = mean(ChanFFT(p:utils.FFTSize))*(utils.SlideWindow/(utils.FFTSize-p));
                 end
                 if SlideMean < (ChanMax/pow2(utils.TGain/6))
-                    ProbFreq = ProbFreq + p*InputSR/2/1024;
+                    ProbFreq = (InputSR/2)/(p/utils.FFTSize);
                     break
                 end
             end
             % Check & fintune
             if utils.EdgeFreq < 1
-                utils.EdgeFreq = round(SrcFreq);
+                utils.EdgeFreq = round(DstFreq);
             end
             if ProbFreq > 5000
-                ProbFreq = ProbFreq-700;
+                ProbFreq = ProbFreq-7000;
             end
             if ProbFreq < 1
                 return
             end
             ProbFreq = round(ProbFreq/2);
-            utils.EdgeFreq = ProbFreq;
+            % Update EdgeFreq
+            NewEdgeFreq = utils.EdgeFreq*0.25+ProbFreq*0.75;
+            utils.EdgeFreq = NewEdgeFreq;
         end
         
         function Output = ModMonoSignal(~,Input,InputSR,SrcFreq,DstFreq)
             % Ref freq range
-            FreqBegin = round(0.5+2*size(Input,1)*DstFreq/InputSR);
-            FreqEnd = round(FreqBegin+0.025*size(Input,1));
+            FreqBegin = round(0.5+size(Input,1)*DstFreq/InputSR);
+            FreqEnd = round(FreqBegin+0.0125*size(Input,1));
             % Src signal power
             SrcAbsSignal = abs(fft(Input));
             SrcPower = mean(SrcAbsSignal(FreqBegin:FreqEnd),'all');
             % Highpass filter
-            % [z,p] = butter(3,2*SrcFreq/InputSR,'high');
-            % HighSignal = filtfilt(z,p,Input);
-            HighSignal = Input;
-            % Move freq
-            MoveRange = round(size(Input,1)*(DstFreq-SrcFreq)/(InputSR/2));
-            MoveSignal = circshift(fft(HighSignal),MoveRange);
-            MoveSignal(1:round(size(Input,1)*DstFreq/(InputSR/2)-1)) = 0;
+            [b,a] = butter(3,SrcFreq/(InputSR/2),'high');
+            HighSignal = filter(b,a,Input);
+            % Move Freq - Right
+            SrcPos = round(SrcFreq*size(Input, 1)/InputSR);
+            DstPos = round(DstFreq*size(Input, 1)/InputSR);
+            EndPos = round(size(Input,1)/2);
+            MoveSignal = fft(HighSignal);
+            MoveSignal(SrcPos:SrcPos+EndPos-DstPos) = MoveSignal(DstPos:EndPos);
+            MoveSignal(1:DstPos) = 0;
+            % Move Freq -Left
+            SrcPos = round(size(Input,1)-SrcPos);
+            DstPos = round(size(Input,1)-DstPos);
+            MoveSignal(EndPos:DstPos) = MoveSignal(SrcPos-DstPos+EndPos:SrcPos);
+            MoveSignal(SrcPos:end) = 0;
             % Move power
             MoveAbsSignal = abs(MoveSignal);
             MovePower = mean(MoveAbsSignal(FreqBegin:FreqEnd),'all');
             % Calc gain
             Gain = SrcPower/MovePower;
+            % Post highpass filter
+            [b,a] = butter(5,DstFreq/(InputSR/2),'high');
             % Output results
-            Output = Gain.*real(ifft(MoveSignal));
+            Output = filter(b,a,Gain.*real(ifft(MoveSignal)));
         end
     end
     
     methods
         function utils = SignalUtils()
             utils.EdgeFreq = 0;
-            utils.ChanSum = complex(zeros(utils.ChanSize,1));
+            utils.ChanSum = complex(zeros(utils.FFTSize*2,1));
             utils.AccCount = 0;
         end
         
@@ -107,7 +122,7 @@ classdef SignalUtils
             end
         end
         
-        function Output = CopyBand(utils,Input,InputSR,SrcFreq,DstFreq,DynPtc)
+        function Output = CopyBand(utils,Input,InputSR,SrcFreq,DstFreq,DynPtc,OnlyModSignal)
             % Handle error
             if SrcFreq >= DstFreq || SrcFreq < 1 || DstFreq < 1 || SrcFreq > (InputSR/2) || DstFreq > (InputSR/2)
                 Output = Input;
@@ -126,31 +141,36 @@ classdef SignalUtils
             % Modulation
             ModOutput = utils.ModMonoSignal(MidChan,InputSR,SrcFreq,DstFreq);
             % Dynamic Protect
+            SrcF = 1.0;
             if DynPtc == true
                 AdpPower = mean(abs(ModOutput),'all');
                 SrcPower = mean(abs(MidChan),'all');
-                SrcF = 1-(AdpPower/SrcPower);
+                if AdpPower/SrcPower < 1
+                    SrcF = 1-(AdpPower/SrcPower);
+                end
+            end
+            % Debug
+            if OnlyModSignal
+                MidChan = ModOutput;
+            else
                 MidChan = ModOutput + SrcF.*MidChan;
             end
             % Return results
             if size(Input,2) == 1
                 Output = MidChan;
             else
-                Output = [MidChan+SideChan, MidChan-SideChan];
+                if OnlyModSignal
+                    Output = [MidChan, MidChan];
+                else
+                    Output = [MidChan+SideChan, MidChan-SideChan];
+                end
             end
         end
         
-        function utils = ResetEdgeFreq(utils)
-            utils.EdgeFreq = 0;
-            utils.ChanSum = complex(zeros(utils.ChanSize,1));
-            utils.AccCount = 0;
-        end
-        
-        function EdgeFreq = GetEdgeFreq(utils,Input,InputSR,SrcFreq)
-            utils = EnvelopeDetect(utils,Input,InputSR,SrcFreq);
+        function EdgeFreq = GetEdgeFreq(utils,Input,InputSR,DstFreq)
+            utils.EnvelopeDetect(Input,InputSR,DstFreq);
             EdgeFreq = utils.EdgeFreq;
         end
-        
     end
 end
 
